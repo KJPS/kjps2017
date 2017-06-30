5f05627fdfc8dccd2879665c154be75b04db94e7
Jānis Gruzis <jgruzis@gmail.com>
2017-06-30 03:47:18 +0300

    Update lib.js

--- lib.js
+++ lib.js
@@ -42,3 +42,52 @@ function getGhosts(gameState)
     
     return ghosts;
 }
+
+/**
+ * Iegūt spēles punktu tabulu.
+ */
+function getPointTable(gameState)
+{
+    return gameState.points;
+}
+
+/**
+ * Iegūt sava spēlētāja datus.
+ */
+function getMyPlayer(gameState)
+{
+    return gameState.my_player;
+}
+
+function canIMoveTo(gameState, x, y)
+{
+    var myPlayer = gameState.my_player;
+    
+    if (x < 0 || x >= gameState.width
+        || y < 0 || y >= gameState.height
+    ) {
+        return false;
+    }
+    
+    var movements = [
+        [0, -1],
+        [1, 0],
+        [0, 1],
+        [-1, 0]
+    ];
+    var way = null;
+    
+    for (var i = 0; i < movements.length; ++i) {
+        var m = movements[i];
+        if (myPlayer.x + m[0] == x && myPlayer.y + m[1] == y) {
+            way = i;
+            break;
+        }
+    }
+    
+    if (way === null) {
+        return false;
+    }
+    
+    return !(game.maze[myPlayer.y][myPlayer.x] & (1 << way));
+}
